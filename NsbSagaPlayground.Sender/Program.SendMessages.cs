2a33223a39116f59a5d2283dc8a503d298a38a89
rioka <rioka@libero.it>
2023-04-27 16:17:27 +0100

    Implement Sender endpoint

---
+++ NsbSagaPlayground.Sender/Program.SendMessages.cs
@@ -0,0 +1,52 @@
+﻿using NsbSagaPlayground.Shared.Messages.Commands;
+using NServiceBus;
+
+internal partial class Program
+{
+  private static async Task SendMessages(IMessageSession session)
+  {
+    Console.WriteLine("Press '1' enter to create an order");
+    Console.WriteLine("Press '2' enter to cancel most recent order creation request");
+    Console.WriteLine("Press any key to exit");
+
+    Guid lastOrder = default;
+    var exit = false;
+    
+    while (!exit)
+    {
+      var ch = Console.ReadKey();
+      Console.WriteLine();
+
+      switch (ch.Key)
+      {
+        case ConsoleKey.D1:
+        case ConsoleKey.NumPad1:
+          var createOrder = new CreateOrder() {
+            Id = lastOrder = Guid.NewGuid()
+          };
+          await session.Send("Receiver", createOrder);
+          break;
+
+        case ConsoleKey.D2:
+        case ConsoleKey.NumPad2:
+          if (lastOrder != default)
+          {
+            var cancelOrder = new CancelOrder() {
+              Id = lastOrder
+            };
+            await session.Send("Receiver", cancelOrder);
+            lastOrder = default;
+          }
+          else
+          {
+            Console.WriteLine("No order available for cancellation");
+          }
+          break;
+          
+        default:
+          exit = true;
+          break;
+      }
+    }
+  }
+}
---
+++ NsbSagaPlayground.Sender/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NsbSagaPlayground.Shared.Infrastructure;
+using NServiceBus;
+
+internal partial class Program
+{
+  public static async Task Main(string[] args)
+  {
+    var host = CreateHostBuilder(args)
+      .Build();
+
+    using (host)
+    {
+      await host.StartAsync();
+      var lifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
+      var session = host.Services.GetRequiredService<IMessageSession>();
+
+      await SendMessages(session); 
+
+      lifetime.StopApplication();
+      await host.WaitForShutdownAsync();      
+    }
+  }
+
+  public static IHostBuilder CreateHostBuilder(string[] args)
+  {
+    var hb = Host
+      .CreateDefaultBuilder()
+      .UseConsoleLifetime()
+      .UseNServiceBus(ctx => {
+
+        var endpointConfig = Bootstrapper.Configure("Sender", ctx.Configuration.GetConnectionString("Data"));
+        return endpointConfig;
+      });
+    
+    return hb;
+  }
+}
