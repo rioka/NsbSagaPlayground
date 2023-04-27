451c70b57c2a6bea5c87ba0703166ba94d0c60fe
rioka <rioka@libero.it>
2023-04-27 14:52:47 +0100

    Add Bootstrapper.cs

---
+++ NsbSagaPlayground.Shared/Infrastructure/Bootstrapper.cs
@@ -0,0 +1,38 @@
+using Microsoft.Extensions.Configuration;
+using NServiceBus;
+
+namespace NsbSagaPlayground.Shared.Infrastructure;
+
+public class NsbBootstrapper
+{
+  public static Task<IEndpointInstance> Start(string endpointName, IConfiguration configuration) 
+  {
+    return Endpoint.Start(Configure(endpointName, configuration));
+  }
+
+  internal static EndpointConfiguration Configure(string endpointName, IConfiguration configuration)
+  {
+    var config = new EndpointConfiguration(endpointName);
+
+    config.Conventions().DefiningCommandsAs(t => t.Namespace?.Contains("Messages.Commands") ?? false);
+    config.Conventions().DefiningEventsAs(t => t.Namespace?.Contains("Messages.Events") ?? false);
+
+    var transport = config.UseTransport<SqlServerTransport>();
+    transport
+      .Transactions(TransportTransactionMode.TransactionScope)
+      .DefaultSchema("nsb")
+      .ConnectionString(configuration.GetConnectionString("Data"));
+    
+    var persistence = config.UsePersistence<SqlPersistence>();
+    persistence
+      .SqlDialect<SqlDialect.MsSqlServer>()
+      .Schema("nsb");
+
+    config.AuditProcessedMessagesTo("audit");
+    config.SendFailedMessagesTo("error");
+    
+    config.EnableInstallers();
+
+    return config;
+  }
+}
