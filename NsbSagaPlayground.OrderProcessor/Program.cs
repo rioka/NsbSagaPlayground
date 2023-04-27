e3de4adeff4d991868930db5c1d23454011fcf95
rioka <rioka@libero.it>
2023-04-27 17:39:06 +0100

    Initial code for saga

---
+++ NsbSagaPlayground.OrderProcessor/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using NsbSagaPlayground.Shared;
+using NsbSagaPlayground.Shared.Infrastructure;
+using NServiceBus;
+
+internal class Program
+{
+  public static async Task Main(string[] args)
+  {
+    var host = CreateHostBuilder(args)
+      .Build();
+
+    await host.RunAsync();
+  }
+
+  private static IHostBuilder CreateHostBuilder(string[] args)
+  {
+    var hb = Host
+      .CreateDefaultBuilder()
+      .UseConsoleLifetime()
+      .UseNServiceBus(ctx => {
+
+        var endpointConfig = Bootstrapper.Configure(Endpoints.OrderProcessor, ctx.Configuration.GetConnectionString("Data"));
+        return endpointConfig;
+      });
+    
+    return hb;
+  }
+}
