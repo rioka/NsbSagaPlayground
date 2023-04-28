ba5f69dccbe7c77d7deb3fee2332b87ebe8a349c
rioka <rioka@libero.it>
2023-04-28 14:16:33 +0100

    Add OrderConfirmedHandler

---
+++ NsbSagaPlayground.Sender/Handlers/OrderConfirmedHandler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using NsbSagaPlayground.Shared.Messages.Events;
+using NServiceBus;
+
+namespace NsbSagaPlayground.Sender.Handlers;
+
+public class OrderConfirmedHandler : IHandleMessages<OrderConfirmed>
+{
+  private readonly ILogger<OrderConfirmedHandler> _logger;
+
+  public OrderConfirmedHandler(ILogger<OrderConfirmedHandler> logger)
+  {
+    _logger = logger;
+  }
+  /// <inheritdoc />
+  public Task Handle(OrderConfirmed message, IMessageHandlerContext context)
+  {
+    _logger.LogInformation("Order {Id} has been cancelled", message.Id);
+    return Task.CompletedTask;
+  }
+}
