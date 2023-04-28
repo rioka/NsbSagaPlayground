992a2d8a60c51bb1c3e84357f5a62842b8a9c7b8
rioka <rioka@libero.it>
2023-04-28 14:59:56 +0100

    Custom behavior to use the connection from persistence in `AppDbContext`

---
+++ NsbSagaPlayground.OrderProcessor/Infrastructure/Behaviors/ForceConnectionFeature.cs
@@ -0,0 +1,12 @@
+﻿using NServiceBus.Features;
+
+namespace NsbSagaPlayground.OrderProcessor.Infrastructure.Behaviors;
+
+internal class ForceConnectionFeature : Feature
+{
+  /// <inheritdoc />
+  protected override void Setup(FeatureConfigurationContext context)
+  {
+    context.Pipeline.Register(new SetConnectionBehavior(), "DbContext uses same connection as persistence");
+  }
+}
---
+++ NsbSagaPlayground.OrderProcessor/Infrastructure/Behaviors/SetConnectionBehavior.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NsbSagaPlayground.Persistence;
+using NServiceBus;
+using NServiceBus.Pipeline;
+
+namespace NsbSagaPlayground.OrderProcessor.Infrastructure.Behaviors;
+
+internal class SetConnectionBehavior : Behavior<IInvokeHandlerContext>
+{
+  public override async Task Invoke(IInvokeHandlerContext context, Func<Task> next)
+  {
+    var dbContext = context.Builder.Build<AppDbContext>();
+    var connection = context.SynchronizedStorageSession.SqlPersistenceSession().Connection;
+    dbContext.Database.SetDbConnection(connection);
+    
+    await next();
+  }
+}
