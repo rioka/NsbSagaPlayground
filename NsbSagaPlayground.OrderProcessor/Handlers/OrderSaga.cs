83798e42b886c294766ffa82be52df1e506a1e83
rioka <rioka@libero.it>
2023-04-28 15:00:17 +0100

    Persist data in `OrderSaga`

--- NsbSagaPlayground.OrderProcessor/Handlers/OrderSaga.cs
+++ NsbSagaPlayground.OrderProcessor/Handlers/OrderSaga.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NsbSagaPlayground.Persistence;
+using NsbSagaPlayground.Shared.Domain;
 using NsbSagaPlayground.Shared.Messages.Commands;
 using NsbSagaPlayground.Shared.Messages.Events;
 using NServiceBus;
@@ -10,10 +13,12 @@ internal class OrderSaga : Saga<OrderData>,
   IHandleMessages<CancelOrder>,
   IHandleTimeouts<BuyerRemorseExpired>
 {
+  private readonly AppDbContext _dbContext;
   private readonly ILogger<OrderSaga> _logger;
 
-  public OrderSaga(ILogger<OrderSaga> logger)
+  public OrderSaga(AppDbContext dbContext, ILogger<OrderSaga> logger)
   {
+    _dbContext = dbContext;
     _logger = logger;
   }
   
@@ -29,20 +34,25 @@ internal class OrderSaga : Saga<OrderData>,
     _logger.LogInformation("Processing {Message}", nameof(CreateOrder));
 
     // TODO insert order
+    _dbContext.Orders.Add(Order.Create(message.Id));
+    await _dbContext.SaveChangesAsync();
+    
     await RequestTimeout<BuyerRemorseExpired>(context, TimeSpan.FromMinutes(1));
   }
 
-  public Task Handle(CancelOrder message, IMessageHandlerContext context)
+  public async Task Handle(CancelOrder message, IMessageHandlerContext context)
   {
     // if CancelOrder is received after BuyerRemorseExpired, nothing will happens
     // because the saga is complete
     // See https://docs.particular.net/tutorials/nservicebus-sagas/2-timeouts/
     _logger.LogInformation("Processing {Message}", nameof(CancelOrder));
 
+    var order = await _dbContext.Orders.SingleAsync(o => o.UId == message.Id);
+    order.Cancel();
+    await _dbContext.SaveChangesAsync();
+    
     _logger.LogInformation("Order {Id} cancelled", message.Id);
     MarkAsComplete();
-    
-    return Task.CompletedTask;
   }
 
   /// <inheritdoc />
@@ -50,7 +60,10 @@ internal class OrderSaga : Saga<OrderData>,
   {
     _logger.LogInformation("Grace period to cancel order {Id} has expired: order is confirmed", Data.OrderId);
     
-    // TODO update order (ConfirmedAt)
+    var order = await _dbContext.Orders.SingleAsync(o => o.UId == Data.OrderId);
+    order.Confirm();
+    await _dbContext.SaveChangesAsync();
+    
     await context.Publish(new OrderConfirmed() {
       Id = Data.OrderId
     });
